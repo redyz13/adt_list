8c97a52ff855c6b94ede4b5614964e287d07f8af
Antonio <35410603+redyz13@users.noreply.github.com>
2022-04-17 21:20:35 +0200

    modified:   src/main.c

--- src/main.c
+++ src/main.c
@@ -13,31 +13,16 @@ int main(void) {
         addFirst(list, i);
     }
 
-    //printList(list);
-    
     list = reverseList(list);
-
-    //putchar('\n');
 
     printList(list);
 
-    putchar('\n');
-
     printf("\nContained: %d\n\n", contains(list, 5));
 
-    //removeItem(list, 2);
-    //removeItem(list, 3);
-    //removeItem(list, 4);
-    //addFirst(list, 3);
-    
-
-    //removeItemPos(list, 0);
-    //removeItemPos(list, 0);
-
-    //removeItemPos(list, 0);
-    //removeItemPos(list, 0);
-    
     list2 = clone(list);
+
+    removeItem(list2, 0);
+    removeItemPos(list2, 3);
 
     printList(list2);
     
